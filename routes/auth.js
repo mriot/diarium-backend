d23a3f767072b61d0ca37331370c9c94c35e1747
Markus Kremer <mkremer@seibert-media.net>
2020-01-02 13:17:22 +0100

    increase readability

--- routes/auth.js
+++ routes/auth.js
@@ -33,7 +33,9 @@ router.post("/", (req, res) => {
 				return;
 			}
 
-			jwt.sign({ username: user.dataValues.username }, process.env.JWT_SECRET, { expiresIn: "1d" }, (err, token) => {
+			jwt.sign({
+				username: user.dataValues.username
+			}, process.env.JWT_SECRET, { expiresIn: "1d" }, (err, token) => {
 				if (err) {
 					res.status(500).json({ error: "Some server sided error occured" });
 					return;
