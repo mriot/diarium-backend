441705b649d13d71cbb9ca2c4f470b2059a33f20
Markus Kremer <mkremer@seibert-media.net>
2020-01-02 13:17:09 +0100

    add function description

--- config/jwt.js
+++ config/jwt.js
@@ -1,5 +1,11 @@
 const jwt = require("jsonwebtoken");
 
+/**
+ * ExpressJS middleware to verify JSON Web Tokens (JWT)
+ * @param {Object} req - ExpressJS request object
+ * @param {Object} res - ExpressJS response object
+ * @param {callback} next - Call the next middleware function in the stack
+ */
 const verifyJWT = (req, res, next) => {
 	const token = (req.headers.authorization && req.headers.authorization.split(" ")[1]) || false;
 
