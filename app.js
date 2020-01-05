274417f9aacb9caa8cb1585a236fc3e82e8644bd
Markus Kremer <mkremer@seibert-media.net>
2020-01-05 21:28:53 +0100

    add logger middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.clear();
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const moment = require("moment");
 const db = require("./config/database");
 
 const port = process.env.PORT || 5000;
@@ -14,6 +15,25 @@ db.authenticate()
 	.catch(err => console.error("Unable to connect to the database:", err));
 
 // Middleware
+app.use((req, res, next) => {
+	// console.log(req.headers);
+	// console.log(req.body);
+	// console.log(req.method);
+	// console.log(req.baseUrl);
+	// console.log(req.url);
+	// console.log(req.params);
+	// console.log(req.query);
+
+	console.log("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖");
+	console.log(`\n📨 ${req.method} REQUEST ON ENDPOINT ${req.baseUrl + req.url} • ${moment().format("YYYY-MM-DD HH:mm:ss")}`);
+	console.log("———————————————————————————————————————————————————————————————————————————————————————————————————————————");
+	console.log("▶ HEADERS:\n", req.headers);
+	console.log("▶ PARAMS:\n", req.params);
+	console.log("▶ QUERY:\n", req.query);
+	console.log("▶ BODY:\n", req.body);
+
+	next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
