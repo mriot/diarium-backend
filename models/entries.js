1bec5e9f1ea1bfab4c5090a59164ba30d63bd8de
Markus Kremer <mkremer@seibert-media.net>
2020-01-10 18:24:39 +0100

    fix scrambled order in final JSON

--- models/entries.js
+++ models/entries.js
@@ -1,21 +1,29 @@
 const Sequelize = require("sequelize");
 const db = require("../config/database");
 
+// NOTE: I've added getters to all of them — even though we only need it on "tags"
+// I did this to fix the scrambled order in the final JSON. (I know it doesn't really matter 😬 )
+// I'm not sure why this happens, probably async stuff (?)
+
 const Entry = db.define("entry", {
 	id: {
 		type: Sequelize.INTEGER,
 		primaryKey: true,
 		autoIncrement: true,
+		get() { return this.getDataValue("id"); },
 	},
 	assignedDay: {
-		type: Sequelize.DATEONLY
+		type: Sequelize.DATEONLY,
+		get() { return this.getDataValue("assignedDay"); },
 	},
 	content: {
 		type: Sequelize.TEXT,
+		get() { return this.getDataValue("content"); },
 	},
 	contentType: {
 		type: Sequelize.STRING,
 		defaultValue: "text/markdown",
+		get() { return this.getDataValue("contentType"); },
 	},
 	tags: {
 		type: Sequelize.STRING,
