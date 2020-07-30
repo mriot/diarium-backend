759432b0bfc090a6e7a32ae9ffd6c92c8ec373a7
Markus Kremer <mkremer@seibert-media.net>
2020-07-30 19:28:54 +0200

    add comment to clarify what the middleware does

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 	next();
 });
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // http://expressjs.com/en/4x/api.html#express.json
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
