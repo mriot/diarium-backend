7e90ea465cfdf2cd986d9e0e0d5674e5e530800e
Markus Kremer <mkremer@seibert-media.net>
2020-10-29 21:01:25 +0100

    disable SQL query logging

--- config/database.js
+++ config/database.js
@@ -2,5 +2,6 @@ const Sequelize = require("sequelize");
 
 module.exports = new Sequelize({
 	dialect: "sqlite",
-	storage: "db.sqlite"
+	storage: "db.sqlite",
+	logging: false // disable logging; default: console.log
 });
