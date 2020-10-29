deb93446a27c2018f2852a916cd42c24b07909c0
Markus Kremer <mkremer@seibert-media.net>
2020-10-29 21:01:50 +0100

    improve logging and add better status codes and messages

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,4 +1,6 @@
 const jwt = require("jsonwebtoken");
+const HttpStatus = require("http-status-codes");
+const chalk = require("chalk");
 
 /**
  * ExpressJS middleware to verify JSON Web Tokens (JWT)
@@ -9,26 +11,30 @@ const jwt = require("jsonwebtoken");
 const verifyJWT = (req, res, next) => {
 	const token = (req.headers.authorization && req.headers.authorization.split(" ")[1]) || false;
 
+	// NO TOKEN FOUND
 	if (!token) {
-		res.sendStatus(401);
+		res.status(HttpStatus.StatusCodes.BAD_REQUEST).json({ error: "No token found in the authorization header" });
+		chalk.red("verifyJWT: No token found!");
 		return;
 	}
 
 	jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
 		if (err) {
-			res.status(401).json({ err });
+			res.status(HttpStatus.StatusCodes.UNAUTHORIZED).json({ error: "Could not verify token" });
+			chalk.red("verifyJWT: Could not be verified!");
 			return;
 		}
 
 		// "refresh" the valid token (=> same payload, new 'exp' and 'iat' date)
-		// NOTE: the old token stays valid until it reaches it's expiry date
+		// IMPORTANT: the old token stays valid until it reaches it's expiry date
 		const payload = decoded;
 		delete payload.exp;
 		delete payload.iat;
 
 		jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "12h" }, (signErr, newToken) => {
 			if (signErr) {
-				res.status(500).json({ error: "Some server sided error occured" });
+				res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Some server sided error occured" });
+				chalk.red("verifyJWT: Failed signing JWT!");
 				return;
 			}
 
