9ad7acba2ad2e73cca380ffd31ebb242dcd0c5bd
Markus Kremer <mkremer@seibert-media.net>
2020-11-24 23:05:05 +0100

    rewriting the sanitizer for tinymce (wip)

--- src/routes/_functions/sanitizer.js
+++ src/routes/_functions/sanitizer.js
@@ -1,76 +1,62 @@
+const HTMLParser = require("node-html-parser");
+const sanitizeHtml = require("sanitize-html");
 const { performance } = require("perf_hooks");
-const logger = require("node-color-log");
+const chalk = require("chalk");
 
 /**
- * function to extraxt texts from Editor.js' JSON data
+ * function to extraxt texts from HTML markup
  * which will be used only for searching and indexing (in DB)
- * @param {object} content - Editor.js JSON content
- * @returns {string} sanitizedContent (or empty string)
+ * @param {string} markup
+ * @returns {string} clean text (or empty string)
  */
 
 const sanitize = content => {
-  if (!content.blocks) {
-    logger.warn("Sanitizer received malformed JSON!");
-    return "";
-  }
-  if (content.blocks.length === 0) {
-    logger.warn("Sanitizer received 0 blocks to sanitize!");
+  if (!content) {
+    console.log(chalk.yellow("Sanitizer received no markup!"));
     return "";
   }
 
   const START = performance.now();
 
-  /**
-   * the regex, explained!
-   * (<([^>]+)>) => html tags
-   * (&lt;.+?&gt;) => html tags written as text
-   * (&\w[^\s;]*;) => escaped html tags
-   * ([,.()[\]{}]) => "custom" characters to remove
-   */
-  const SANI_REGEX = /(<([^>]+)>)|(&lt;.+?&gt;)|(&\w[^\s;]*;)|([,.:()[\]{}])/gi;
-
-  const extractTextFromContent = jsonContent => {
-    /* filter out blocks that are either...
-      * on the blacklist or
-      * don't have a data object or
-      * whose data object is empty
-    */
-    const typeBlacklist = ["delimiter"];
-    const blocks = jsonContent.blocks.filter(block => (
-      !typeBlacklist.includes(block.type) &&
-      block.data &&
-      Object.getOwnPropertyNames(block.data).length > 0
-    ));
-
-    const texts = [];
-    for (let i = 0; i < blocks.length; i++) {
-      // TEXT property
-      if (blocks[i].data.text) {
-        texts.push(blocks[i].data.text);
-        continue;
-      }
-      // LIST ITEMS
-      if (blocks[i].data.items) {
-        texts.push(...blocks[i].data.items);
-        continue;
-      }
-      // CAPTIONS
-      if (blocks[i].data.caption) {
-        texts.push(blocks[i].data.caption);
-        continue;
-      }
+  const PARSED_HTML = HTMLParser.parse(content, {
+    lowerCaseTagName: false, // convert tag name to lower case (hurt performance heavily)
+    comment: false, // retrieve comments (hurt performance slightly)
+    blockTextElements: { // keep text content when parsing
+      script: false,
+      noscript: false,
+      style: false,
+      pre: true
     }
-    return texts.join(" ");
-  };
+  });
 
-  const extractedText = extractTextFromContent(content);
-  const sanitizedContent = extractedText.replace(SANI_REGEX, "");
+  let cleanHTML = "";
+  let cleanText = "";
+
+  try {
+    // cleanHTML = PARSED_HTML.querySelectorAll("html, head, body");
+    cleanHTML = sanitizeHtml(content, {
+      allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"])
+    });
+    console.log("ORIGINAL CONTENT");
+    console.log(content);
+    console.log("———————————————————");
+    console.log("CLEANED HTML");
+    console.log(cleanHTML);
+  } catch (error) {
+    console.log(chalk.red(error));
+  }
+
+  try {
+    cleanText = PARSED_HTML.removeWhitespace().structuredText.replace(/\r?\n|\r/g, " ");
+  } catch (error) {
+    console.log(chalk.red(error));
+  }
 
   const END = performance.now();
 
-  logger.info(`Sanitizing ${content.blocks.length} blocks took ${(END - START).toFixed(2)} ms`);
+  console.log(chalk.cyan(`Sanitizing ${content.length} chars took ${(END - START).toFixed(2)} ms`));
 
-  return sanitizedContent;
+  return cleanText;
 };
 
 module.exports = sanitize;
