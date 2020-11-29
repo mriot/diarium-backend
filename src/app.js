fcf0937f9d12214e7ec2d949f3f6c907e88f925a
Markus Kremer <mkremer@seibert-media.net>
2020-11-29 22:32:42 +0100

    increase payload limit to 500kb

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors());
-app.use(express.json()); // http://expressjs.com/en/4x/api.html#express.json
+app.use(express.json({ limit: "500kb" })); // http://expressjs.com/en/4x/api.html#express.json
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
