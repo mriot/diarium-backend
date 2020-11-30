42e4a9165efc6d73007efea1fdbdc94d9aac1327
Markus Kremer <mkremer@seibert-media.net>
2020-11-30 20:48:52 +0100

    decrease limit to 300kb; add content length logging

--- src/app.js
+++ src/app.js
@@ -26,7 +26,10 @@ app.use((req, res, next) => {
       "•",
       dayjs().format("YYYY-MM-DD HH:mm:ss"),
       "•",
-      chalk.bgCyan(req.method), req.url
+      chalk.bgCyan(req.method), req.url,
+      "•",
+      req.headers["content-length"],
+      "bytes"
     );
 
     // console.log("▶ HEADERS:\n", req.headers);
@@ -45,7 +48,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors());
-app.use(express.json({ limit: "500kb" })); // http://expressjs.com/en/4x/api.html#express.json
+app.use(express.json({ limit: "300kb" })); // http://expressjs.com/en/4x/api.html#express.json
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
