6327ba873c8a541cee57e77257b812ec2cc44bf9
Markus Kremer <mkremer@seibert-media.net>
2020-12-29 22:27:11 +0100

    (wip) upload endpoint

--- src/routes/entries/uploads.js
+++ src/routes/entries/uploads.js
@@ -2,21 +2,69 @@ const dayjs = require("dayjs");
 const express = require("express");
 const verifyJWT = require("../../middleware/jwt");
 const { StatusCodes } = require("http-status-codes");
+const fs = require("fs");
+const path = require("path");
 
 const router = express.Router();
-const PATH = process.env.LOCAL_UPLOAD_TARGET;
+const ROOT = process.env.LOCAL_UPLOAD_TARGET;
+
+/**
+ * /uploads => all uploads
+ * /uploads/2020 => all uploads for 2020
+ * ...
+ * /uploads/2020/12/02/filename.jpg => Single file
+ *
+ * Multiple uploads are returned as array and sorted by date
+ *
+ */
+
+/*
+example: /uploads/2020/12/
+[
+  { date: "2020-12-02", type: "image/jpeg", url: "/api/entries/uploads/2020/12/02/picture.jpg" },
+  { date: "2020-12-10", type: "image/jpeg", url: "/api/entries/uploads/2020/12/10/picture.jpg" },
+  { date: "2020-12-20", type: "image/jpeg", url: "/api/entries/uploads/2020/12/20/picture.jpg" }
+]
+
+example: /uploads/2020/
+{
+  12: [
+    { date: "2020-12-02", type: "image/jpeg", url: "/api/entries/uploads/2020/12/02/picture.jpg" },
+    { date: "2020-12-10", type: "image/jpeg", url: "/api/entries/uploads/2020/12/10/picture.jpg" },
+    { date: "2020-12-20", type: "image/jpeg", url: "/api/entries/uploads/2020/12/20/picture.jpg" }
+  ]
+}
+*/
 
 router.get("/uploads/:year/:month/:day/:filename", function (req, res) {
-  const image = `${PATH}/2020-12-01/Coronamarkus_2.jpg`;
-  const video = `${PATH}/2020-12-01/hover.mp4`;
-  const pdf = `${PATH}/2020-12-01/test.pdf`;
-  const txt = `${PATH}/2020-12-01/StarWars_Movie_Order.txt`;
-  const audio = `${PATH}/2020-12-01/audio.mp3`;
+  const { year, month, day, filename } = req.params;
+  const parsedDate = dayjs(`${year}/${month}/${day}`, "YYYY/MM/DD", true);
 
-  res.sendFile(image);
+  if (!parsedDate.isValid()) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      error: parsedDate.toString(),
+      required_format: "YYYY/MM/DD"
+    });
+    return;
+  }
+
+  const filepath = path.join(ROOT, parsedDate.format("YYYY-MM-DD"), filename);
+
+  fs.open(filepath, "r", (err, fd) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(StatusCodes.NOT_FOUND);
+    }
+
+    res.sendFile(filepath);
+
+    fs.close(fd, (err) => {
+      if (err) throw err;
+    });
+  });
 });
 
-router.post("/uploads/:year?/:month?/:day?", verifyJWT, function (req, res) {
+router.post("/uploads/:year?/:month?/:day?", function (req, res) {
   const { year, month, day } = req.params;
   const parsedDate = dayjs(`${year}/${month}/${day}`, "YYYY/MM/DD", true);
 
@@ -32,17 +80,15 @@ router.post("/uploads/:year?/:month?/:day?", verifyJWT, function (req, res) {
     return res.status(StatusCodes.BAD_REQUEST).json({
       code: StatusCodes.BAD_REQUEST,
       error: "Bad Request",
-      message: "No file to upload found"
+      message: "No file to upload"
     });
   }
 
   const file = req.files.file;
 
-  console.log(file);
-  const dest = `${PATH}/${parsedDate.format("YYYY-MM-DD")}/${file.name.replace(/\s/, "_")}`;
-  console.log(dest);
+  const filepath = path.join(ROOT, parsedDate.format("YYYY-MM-DD"), file.name.replace(/\s/, "_"));
 
-  file.mv(dest, (err) => {
+  file.mv(filepath, (err) => {
     if (err) {
       return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
         code: StatusCodes.INTERNAL_SERVER_ERROR,
@@ -52,7 +98,9 @@ router.post("/uploads/:year?/:month?/:day?", verifyJWT, function (req, res) {
       });
     }
 
-    res.send("File uploaded!");
+    res.json({
+      path: path.join(req.baseUrl, req.path, file.name.replace(/\s/, "_"))
+    });
   });
 });
 
